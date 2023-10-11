30c1820f36ccbb9611c23a880331de936b43dd9e
Miguel Matos <miguelmatosalexandre98@gmail.com>
2023-10-11 22:55:05 +0100

    My Audio System
    Prototyping class structure and member's initialization

---
+++ SoundSystem.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SoundSystem
+{
+private:
+	int _flags;
+
+	int _initted;
+
+	int _audioChannell;
+
+	int _extraRepetition;
+
+	vector<string> AudioCollection;
+public:
+	SoundSystem();
+
+	LoadSound()
+};
+
