ba6fa47423f547225474cef9925c0c8a79f606f2
Vasco Almeida <l.vasco.m.a21@gmail.com>
2023-11-10 18:40:56 +0000

    lvl system start

--- Level.cpp
+++ Level.cpp
@@ -1,1 +1,29 @@
 #include "Level.h"
+#include "Core/DeltaTime.h"
+#include "Rendering/RenderInstanceManager.h"
+
+Level::Level(const std::string& levelId, Player* player, TileMap* tileMap)
+{
+	_levelId = levelId;
+	_player = player;
+	_tileMap = tileMap;
+
+
+	GameRenderer* renderer = RenderInstanceManager::instance().GetRenderer("main");
+	renderer->SetObjectToTrack(_player);
+}
+
+void Level::Update()
+{
+	DeltaTime::UpdateDeltaTime();
+
+
+}
+
+void Level::Render()
+{
+}
+
+void Level::Clean()
+{
+}
--- Level.h
+++ Level.h
@@ -8,6 +8,7 @@ class TileMap;
 
 class Level
 {
+
 private:
 	Player* _player;
 	TileMap* _tileMap;
@@ -17,11 +18,21 @@ private:
 	std::string _levelId;
 	bool _isActive;
 
+public:
 
-	void Init();
+	Level(const std::string& levelId, Player* player, TileMap* tileMap);
+	~Level();
+
 	void Update();
 	void Render();
 	void Clean();
+
+#pragma region Getters/Setters
+	std::string GetLevelId() { return _levelId; }
+	void SetLevelId(std::string levelId) { _levelId = levelId; }
+
+	bool IsActive() { return _isActive; }
+#pragma endregion
 
 };
 
