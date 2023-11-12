19c0d76794b0cd488188eeb1edbd06cabf489f62
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-11-12 16:36:28 +0000

    Working on pool system

--- main.cpp
+++ main.cpp
@@ -4,20 +4,18 @@
 
 int main(int argc, char* argv[])
 {
-    //bool running = Game::instance().Init();
-    //if (!running)
-    //{
-    //    std::cout << "Game::Init failed" << std::endl;
-    //    return -1;
-    //}
-    //
-    //while (running)
-    //{
-    //    Game::instance().Update();
-    //    running = Game::instance().IsRunning();
-    //}
-
-    ObjectSerializer::Example();
+    bool running = Game::instance().Init();
+    if (!running)
+    {
+        std::cout << "Game::Init failed" << std::endl;
+        return -1;
+    }
+    
+    while (running)
+    {
+        Game::instance().Update();
+        running = Game::instance().IsRunning();
+    }
 
     return 0;
 }
