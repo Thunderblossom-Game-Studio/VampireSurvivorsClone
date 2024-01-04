677b56c1af0c94a4afcb07212564aa9fe4ad0394
DENraynaH <103390897+DENraynaH@users.noreply.github.com>
2024-01-04 19:04:46 +0000

    enemy spawning finshing

--- GameObjects/EnemySpawner.h
+++ GameObjects/EnemySpawner.h
@@ -3,19 +3,24 @@
 #include "BaseGameObject.h"
 #include "ExampleGameObject.h"
 #include "Player.h"
-
-class EnemySpawner final : public BaseGameObject
+class EnemySpawner : public BaseGameObject
 {
 
 private:
-    float _spawnerRadius = 0;
-    float _spawnRate = 0;
-    float _spawnTimer = 0;
+    float _spawnerRadius = 0.0f;
     BaseGameObject* _objectSpawnReference;
+    float _spawnRate = 5.0f;
+    float _spawnTimer = 0.0f;
+    std::vector<class Enemy*> _currentEnemies;
     
 public:
     Vector2 GetRandomPointInCircle(Vector2 circlePosition) const;
-    void SpawnObject() const;
+    Vector2 GetSpawnPosition() const;
+    void SpawnEnemy();
+    void Clean();
+    void Update(float deltaTime) override;
+    void LateUpdate(float deltaTime) override;
+    
     EnemySpawner(BaseGameObject* objectSpawnReference, float spawnerRadius, float spawnRate);
     ~EnemySpawner() override;
 };
